5deacacb889c399e89d73e8c709218e2a5aaec8b
hana <hana.catic@epfl.ch>
2024-10-01 18:50:23 +0200

    epsilon experiments

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ def beta_experiments(delta = 0.25):
     plot_payoff(game.gameSetup.payoff, folder = folder, save = save)
     mean_potential_history = np.zeros((6, game.max_iter))
     betas = np.arange(beta_t/2, beta_t + beta_t/8.0, beta_t/8.0)
+    
     for idx, beta in enumerate(betas):
         print(beta)
         potentials_history = np.zeros((n_exp, game.max_iter))
@@ -50,7 +51,7 @@ def beta_experiments(delta = 0.25):
         plt.pause(20)
         plt.close()
         
-def delta_experiments():
+def delta_experiments(beta = 10, deltas = [0.9, 0.75, 0.5, 0.25, 0.1]):
     
     action_space = [0, 1, 2, 3]
     
@@ -60,16 +61,13 @@ def delta_experiments():
     save = False 
     folder = 'WEEK 4'
     title = 'Average potential'
-    n_exp = 25
-        
-    deltas = [0.9, 0.75, 0.5, 0.25, 0.1]
+    n_exp = 25    
     
     gameSetup = RandomIdenticalInterestGame(action_space, firstNE, secondNE, deltas[0])
     game = Game(gameSetup, mu=mu)
     game.set_initial_action_profile(secondNE)
     beta_t = game.compute_beta(EPS)
     plot_payoff(game.gameSetup.payoff)
-    beta = 10 # game.compute_beta(EPS)
     mean_potential_history = np.zeros((6, game.max_iter))
     
     for idx, delta in enumerate(deltas):
@@ -77,6 +75,7 @@ def delta_experiments():
         print(delta)
         gameSetup.reset_payoff_matrix(delta)
         game.reset_game(gameSetup)
+        plot_payoff(game.gameSetup.payoff)
         beta_t = game.compute_beta(EPS)
         print(beta_t)
         
@@ -97,12 +96,56 @@ def delta_experiments():
         plt.show(block = False)
         plt.pause(20)
         plt.close()
-               
+
+def epsilon_experiments(delta = 0.25, epsilons = [0.2, 0.1, 0.05, 0.01, 0.001]):
+    
+    action_space = [0, 1, 2, 3]
+    
+    firstNE = np.array([1,1])
+    secondNE = np.array([3,3])
+    
+    save = False 
+    folder = 'WEEK 4'
+    title = 'Average potential'
+    n_exp = 25
+            
+    gameSetup = RandomIdenticalInterestGame(action_space, firstNE, secondNE, delta)
+    game = Game(gameSetup, mu=mu)
+    game.set_initial_action_profile(secondNE)
+    plot_payoff(game.gameSetup.payoff)
+    
+    mean_potential_history = np.zeros((10, game.max_iter))
+    
+    for idx, eps in enumerate(epsilons):
+        
+        beta = game.compute_beta(eps)
+        print(beta)
+        
+        potentials_history = np.zeros((n_exp, game.max_iter))
+        for i in range(0, n_exp):
+            potentials_history[i] = np.transpose(game.potentials_history).copy()
+            game.play(beta=beta)
+            
+        mean_potential_history[idx] = np.mean(potentials_history, 0)
+    
+        mean_potential_history[idx + 5] = (1 - eps) * np.ones((1, game.max_iter))
+    labels = [ r'$\epsilon = 0.2$', r'$\epsilon = 0.1$', r'$\epsilon = 0.05$', r'$\epsilon = 0.01$', r'$\epsilon = 0.001$']
+   
+    plot_lines_eps_exp(mean_potential_history, labels, True, title = title, folder = folder, save = save)
+
+    if not save:
+        # plt.show()
+        plt.show(block = False)
+        plt.pause(20)
+        plt.close()
+
 def main():
     
     # beta_experiments(0.25)
     
-    delta_experiments()
+    # delta_experiments()
+    
+    epsilon_experiments()
 
 if __name__ == '__main__':
     
--- plot.py
+++ plot.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 def plot_payoff(payoff, title = "Payoff matrix", folder = None, save = False):
     
-    f = plt.figure(1)
+    f = plt.figure()
     plt.title(title)
     plt.xlabel('Player 2', fontsize=10)
     plt.ylabel('Player 1', fontsize=10)
@@ -17,7 +18,7 @@ def plot_payoff(payoff, title = "Payoff matrix", folder = None, save = False):
 
 def plot_potential(mean_potential, title = 'Average potential', file_name = None, folder = None, save = False):
     
-    f = plt.figure(2)
+    f = plt.figure()
     f.clf()
     plt.plot(mean_potential)
     plt.xlabel('time', fontsize=10)
@@ -32,7 +33,7 @@ def plot_potential(mean_potential, title = 'Average potential', file_name = None
     
 def plot_potential_with_std(mean_potential, std, title = 'Average potential', file_name = None, folder = None, save = False):
     
-    f = plt.figure(3)
+    f = plt.figure()
     f.clf()
     plt.plot(mean_potential)
     plt.fill_between(range(0, len(mean_potential)), mean_potential - std, mean_potential + std,
@@ -57,6 +58,36 @@ def plot_lines(lines_to_plot, list_labels, plot_e_efficient = False, title = 'Av
         elif plot_e_efficient:
             plt.plot(element, 'k--', label =  list_labels[idx])
         
+        
+    # plt.xlabel('Potential', fontsize=15)
+    plt.xlabel('time', fontsize=10)
+    plt.legend(fontsize=15)
+    plt.title(title, fontsize=15)
+    plt.ylim(0, 1.1)
+    plt.legend(loc="lower right")
+    
+    if save:
+        plt.savefig('../' + folder + '/' + file_name + '.png')
+    else:
+        f.show()
+
+def plot_lines_eps_exp(lines_to_plot, list_labels, plot_e_efficient = False, title = 'Average potential', file_name = None, folder = None, save = False):
+    
+    f = plt.figure()
+    f.clf()
+    
+    for idx in range(0, int(len(lines_to_plot)/2)):
+        plt.plot(lines_to_plot[idx ], label = list_labels[idx])
+        plt.plot(lines_to_plot[idx + int(len(lines_to_plot)/2)], 'k--')
+
+    colormap = cm.get_cmap("plasma") # The various colormaps can be found here: https://matplotlib.org/3.1.0/tutorials/colors/colormaps.html
+    ax = plt.gca()
+    lines = ax.lines
+    N = len(lines)
+    for n in range(0, N, 2): # For each two-lines made via `plt.plot(...)`:
+        random_color = colormap(n/N) # This function takes a number between 0 and 1 and returns a color.
+        lines[n].set_color(random_color)
+        lines[n+1].set_color(random_color)
         
     # plt.xlabel('Potential', fontsize=15)
     plt.xlabel('time', fontsize=10)
