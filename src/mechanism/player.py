7291489d94870828319f0a078014bd80b6d6b3b6
Hana Catic <hana.catic@epfl.ch>
2025-05-29 20:27:50 +0200

    renamed variables properties.

--- src/mechanism/player.py
+++ src/mechanism/player.py
@@ -2,9 +2,6 @@ import numpy as np
 
 rng = np.random.default_rng()
 
-# todo - add docstrings to all functions
-# todo - add tests for all functions
-# todo - chosen action sometimes returns a list, sometimes an int 
 class Player:
     """
     Class representing the Player.
@@ -30,21 +27,23 @@ class Player:
         """
         
         self.id = player_id
-        self.no_actions = len(action_space) # size of the actions space
-        self.utility = utility # utility function
-        self.noisy_utility = noisy_utility
-        self.fixed_share = fixed_share
-        self.past_action = None
-        self.action_space = np.arange(self.no_actions).reshape(1, self.no_actions)
-        self.prob = 1/self.no_actions*np.ones([1, self.no_actions])
-        self.weights = 1/self.no_actions*np.ones([1, self.no_actions])
-        self.scores = 1/self.no_actions*np.ones([1, self.no_actions])
+        self.update_player(action_space, utility, noisy_utility, fixed_share)
+
+    def _initialize_player_attributes(self):
+        """
+        Initialize or reset player attributes to their default values.
+        """
+        self.previous_action = None
+        self.action_space = np.arange(self.n_actions).reshape(1, self.n_actions)
+        self.probabilities = 1/self.n_actions*np.ones([1, self.n_actions])
+        self.weights = 1/self.n_actions*np.ones([1, self.n_actions])
+        self.scores = 1/self.n_actions*np.ones([1, self.n_actions])
         self.initial_action = np.array([0])
-        self.ones = np.ones(self.no_actions)
-        self.rewards_estimate = np.zeros(self.no_actions)
+        self.ones_vector = np.ones(self.n_actions)
+        self.rewards_estimate = np.zeros(self.n_actions)
         self.min_payoff = None
         self.max_payoff = None
-        self.past_opponents_actions = None
+        self.previous_opponents_actions = None
         self.utilities = None
         
     def update_log_linear(
@@ -71,25 +70,25 @@ class Player:
         if self.noisy_utility and eta <= 0:
             raise ValueError("Noisy utility requires a positive noise bound (eta).")
 
-        if self.utilities is None or not np.array_equal(self.past_opponents_actions, opponents_actions):
+        if self.utilities is None or not np.array_equal(self.previous_opponents_actions, opponents_actions):
             # compute utilities
-            self.utilities = np.array([self.utility(i, opponents_actions) for i in range(self.no_actions)]).reshape(1, self.no_actions)
+            self.utilities = np.array([self.utility(i, opponents_actions) for i in range(self.n_actions)]).reshape(1, self.n_actions)
             
             if self.min_payoff is not None:
                 self.utilities = (self.utilities - self.min_payoff)/(self.max_payoff - self.min_payoff)
             
-            self.utilities +=  rng.uniform(-eta, eta, self.no_actions)
+            self.utilities +=  rng.uniform(-eta, eta, self.n_actions)
 
             # compute strategy        
             exp_values = np.exp(beta * (self.utilities - np.max(self.utilities)))
-            self.prob = exp_values/np.sum(exp_values)
+            self.probabilities = exp_values/np.sum(exp_values)
             
-            self.prob = gamma/self.no_actions + (1-gamma)*self.prob
-            self.past_opponents_actions = opponents_actions
+            self.probabilities = gamma/self.n_actions + (1-gamma)*self.probabilities
+            self.previous_opponents_actions = opponents_actions
             
-        chosen_action = rng.choice(self.action_space[0], size=1, p=self.prob[0]) # sample action from strategy
+        chosen_action = rng.choice(self.action_space[0], size=1, p=self.probabilities[0])
 
-        self.past_action = chosen_action
+        self.previous_action = chosen_action
         
         return chosen_action
         
@@ -110,21 +109,21 @@ class Player:
         """
         
         # sample trial action
-        new_action = rng.integers(0, self.no_actions, 1).astype(int)[0]
+        new_action = rng.integers(0, self.n_actions, 1).astype(int)[0]
         new_utility = self.utility(new_action, opponents_actions)
         
-        if self.utilities is None or not np.array_equal(self.past_opponents_actions, opponents_actions):
-            self.utilities = self.utility(self.past_action, opponents_actions)
+        if self.utilities is None or not np.array_equal(self.previous_opponents_actions, opponents_actions):
+            self.utilities = self.utility(self.previous_action, opponents_actions)
         
-        actions = [self.past_action, new_action]
+        actions = [self.previous_action, new_action]
         # compute utilities
         utilities = np.array([self.utilities, new_utility])
         # compute strategy
         exp_values = np.exp(beta * (utilities - np.max(utilities)))
-        self.prob = exp_values/np.sum(exp_values)
+        probabilities = exp_values/np.sum(exp_values)
         
-        chosen_action = rng.choice(actions, size=1, p=self.prob.T[0]) # sample action
-        self.past_action = chosen_action[0]
+        chosen_action = rng.choice(actions, size=1, p=probabilities.T[0])
+        self.previous_action = chosen_action[0]
         
         return chosen_action
     
@@ -144,17 +143,17 @@ class Player:
             int: Chosen action.
         """
         
-        if self.utilities is None or not np.array_equal(self.past_opponents_actions, opponents_actions):
+        if self.utilities is None or not np.array_equal(self.previous_opponents_actions, opponents_actions):
             # Compute utilities
-            self.utilities = np.array([self.utility_modified(i, opponents_actions) for i in range(self.no_actions)]).reshape(1, self.no_actions)
+            self.utilities = np.array([self.utility_modified(i, opponents_actions) for i in range(self.n_actions)]).reshape(1, self.n_actions)
             # Compute strategy
             exp_values = np.exp(beta * (self.utilities - np.max(self.utilities)))
-            self.prob = exp_values/np.sum(exp_values)
-            self.past_opponents_actions = opponents_actions
+            self.probabilities = exp_values/np.sum(exp_values)
+            self.previous_opponents_actions = opponents_actions
             
-        chosen_action = rng.choice(self.action_space[0], size=1, p=self.prob[0]) # sample action
+        chosen_action = rng.choice(self.action_space[0], size=1, p=self.probabilities[0])
 
-        self.past_action = chosen_action
+        self.previous_action = chosen_action
         
         return chosen_action
     
@@ -170,23 +169,23 @@ class Player:
         """
         
         # compute utilities
-        if self.utilities is None or not np.array_equal(self.past_opponents_actions, opponents_actions):
+        if self.utilities is None or not np.array_equal(self.previous_opponents_actions, opponents_actions):
                     
-            self.utilities = np.array([self.utility(i, opponents_actions) for i in range(self.no_actions)]).reshape(1, self.no_actions)
+            self.utilities = np.array([self.utility(i, opponents_actions) for i in range(self.n_actions)]).reshape(1, self.n_actions)
 
-            chosen_action = np.argmax(self.utilities) # obtain the utility maximiser
+            chosen_action = np.argmax(self.utilities)
         
-            self.past_action = chosen_action
-            self.past_opponents_actions = opponents_actions
+            self.previous_action = chosen_action
+            self.previous_opponents_actions = opponents_actions
         
-        return self.past_action
+        return self.previous_action
     
     def mixed_strategy(self) -> np.ndarray:
         """
         Returns:
             np.array(A): Strategy.
         """
-        return self.prob
+        return self.probabilities
         
     def update_mw(
         self,
@@ -203,19 +202,19 @@ class Player:
                 
         # compute utilities 
         # Efficiently cache utilities only if opponents' actions have changed
-        if self.utilities is None or not np.array_equal(self.past_opponents_actions, opponents_actions):
+        if self.utilities is None or not np.array_equal(self.previous_opponents_actions, opponents_actions):
             
-            self.utilities = np.array([self.utility(i, opponents_actions) for i in range(self.no_actions)]).reshape(1, self.no_actions)
-            self.past_opponents_actions = opponents_actions
+            self.utilities = np.array([self.utility(i, opponents_actions) for i in range(self.n_actions)]).reshape(1, self.n_actions)
+            self.previous_opponents_actions = opponents_actions
             
             if self.min_payoff is not None:
                 self.utilities = (self.utilities - self.min_payoff)/(self.max_payoff - self.min_payoff)
         
-        losses = self.ones - self.utilities
+        losses = self.ones_vector - self.utilities
         
-        self.prob = np.multiply(self.prob, np.exp(np.multiply(gamma_t, -losses))) # update straetgy
+        self.probabilities = np.multiply(self.probabilities, np.exp(np.multiply(gamma_t, -losses)))
         
-        self.prob = self.prob / np.sum(self.prob)
+        self.probabilities = self.probabilities / np.sum(self.probabilities)
     
     def update_ewa(
         self,
@@ -233,20 +232,20 @@ class Player:
             eps_n (float): Vanishing factor.
         """
 
-        v = np.zeros(self.no_actions)
+        v = np.zeros(self.n_actions)
         v[action] = self.utility(action, opponents_actions)
         
         if self.min_payoff is not None:
             v[action] = (v[action] - self.min_payoff)/(self.max_payoff - self.min_payoff)
         
-        v[action] /= self.prob[0][action]
+        v[action] /= self.probabilities[0][action]
         self.scores += gamma_n * v
 
         exp_values = np.exp((self.scores - np.max(self.scores)))
         lambda_scores = exp_values/np.sum(exp_values)
         
-        self.prob = eps_n*self.ones/self.no_actions + (1-eps_n)*lambda_scores
-        self.prob = self.prob / np.sum(self.prob)
+        self.probabilities = eps_n*self.ones_vector/self.n_actions + (1-eps_n)*lambda_scores
+        self.probabilities = self.probabilities / np.sum(self.probabilities)
         
     def update_exp3p(
         self,
@@ -267,22 +266,22 @@ class Player:
             eta (float): Parameter of EXP3P
         """
         
-        v = np.zeros(self.no_actions)
+        v = np.zeros(self.n_actions)
         v[action] = self.utility(action, opponents_actions)
                 
         if self.min_payoff is not None:
             v[action] = (v[action] - self.min_payoff)/(self.max_payoff - self.min_payoff)
 
-        v[action] = v[action]/self.prob[0][action]
+        v[action] = v[action]/self.probabilities[0][action]
                 
-        self.rewards_estimate = self.rewards_estimate + beta*np.divide(self.ones, self.prob) + v
+        self.rewards_estimate = self.rewards_estimate + beta*np.divide(self.ones_vector, self.probabilities) + v
        
         temp = np.multiply(eta, self.rewards_estimate)
         self.weights  = np.exp(temp - np.max(temp))
         self.weights  = self.weights/np.sum(self.weights)
-        self.prob = (1-gamma)*self.weights + gamma/self.no_actions*self.ones
+        self.probabilities = (1-gamma)*self.weights + gamma/self.n_actions*self.ones_vector
         
-        self.prob = self.prob/np.sum(self.prob)
+        self.probabilities = self.probabilities/np.sum(self.probabilities)
         
     def set_modified_utility(self, utility_modified: callable) -> None:
         """
@@ -297,25 +296,21 @@ class Player:
         """
             Reset initial values of important attributes.
         """
+        self._initialize_player_attributes()
         
-        self.past_action = None
-        self.prob = 1/self.no_actions*np.ones([1, self.no_actions])
-        self.weights = 1/self.no_actions*np.ones([1, self.no_actions])
-        self.scores = 1/self.no_actions*np.ones([1, self.no_actions])
-        self.initial_action = np.array([0])
-        self.ones = np.ones(self.no_actions)
-        self.rewards_estimate = np.zeros(self.no_actions)
-        self.past_opponents_actions = None
-        self.utilities = None
-        
-    def reset_player(self, no_actions: int, utility: callable) -> None:
+    def update_player(self, action_space: np.ndarray, utility: callable, noisy_utility: bool = False, fixed_share: bool = False) -> None:
         """
-            Reset the utility function of a player.
+            Update the player's action space, utility function, and optional parameters.
 
         Args:
-            no_actions (int): Number of actions.
-            utility (function): Utility function
+            action_space (np.ndarray): Player's action space.
+            utility (function): Utility function.
+            noisy_utility (bool, optional): Use noisy utilities. Defaults to False.
+            fixed_share (bool, optional): Use fixed-share log linear learning. Defaults to False.
         """
-
-        self.no_actions = no_actions # size of the actions space
-        self.utility = utility # utility function
+        self.n_actions = len(action_space)
+        self.action_space = np.arange(self.n_actions).reshape(1, self.n_actions)
+        self.utility = utility
+        self.noisy_utility = noisy_utility
+        self.fixed_share = fixed_share
+        self._initialize_player_attributes()
